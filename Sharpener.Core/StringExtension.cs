3cbb8279236737ae8d11ade742b082bf8b9af346
william <william.mcnulty@live.co.uk>
2016-10-12 23:05:10 +0100

    build fix v2

--- Sharpener.Core/StringExtension.cs
+++ Sharpener.Core/StringExtension.cs
@@ -1,10 +1,7 @@
-﻿using JetBrains.Annotations;
-
-namespace Sharpener.Core
+﻿namespace Sharpener.Core
 {
     public static class StringExtension
     {
-        [StringFormatMethod("format")]
         public static string FormatWith(this string format, params object[] args)
         {
             return string.Format(format, args);
