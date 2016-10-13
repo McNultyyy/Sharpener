cd4a5a432bbc5a6f7fb5879abed72a548899e2ae
william <william.mcnulty@live.co.uk>
2016-10-13 19:54:37 +0100

    added Of

--- Sharpener.Core/IntExtension.cs
+++ Sharpener.Core/IntExtension.cs
@@ -8,11 +8,16 @@ namespace Sharpener.Core
     {
         public static IEnumerable<int> To(this int start, int end)
         {
-            var posDiff = Math.Abs(end - start +1);
+            var posDiff = Math.Abs(end - start + 1);
             var negDiff = Math.Abs(start - end + 1);
             if (end > start)
                 return Enumerable.Range(start, posDiff);
             return Enumerable.Range(end, negDiff).Reverse();
+        }
+
+        public static IEnumerable<T> Of<T>(this int amount, T instance)
+        {
+            return 1.To(amount).Select(x => instance);
         }
     }
 }
