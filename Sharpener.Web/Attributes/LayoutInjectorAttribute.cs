27e7e628df677d8ec401913d1be575d425e7c940
McNultyyy <william.mcnulty@live.co.uk>
2016-10-21 15:21:57 +0100

    Create LayoutInjectorAttribute.cs

---
+++ Sharpener.Web/Attributes/LayoutInjectorAttribute.cs
@@ -0,0 +1,24 @@
+using System.Web.Mvc;
+
+namespace Sharpener.Attributes
+{
+    public class LayoutInjectorAttribute : ActionFilterAttribute
+    {
+        private readonly string _layoutName;
+
+        public LayoutInjectorAttribute(string layoutName)
+        {
+            _layoutName = layoutName;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var result = filterContext.Result as ViewResult;
+            if (result != null)
+            {
+                result.MasterName = _layoutName;
+            }
+        }
+    }
+}
