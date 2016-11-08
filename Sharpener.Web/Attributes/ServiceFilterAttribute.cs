dd9e935ec5a83be15b7a76d00004f3fccf8af8fa
McNultyyy <william.mcnulty@live.co.uk>
2016-11-08 18:08:45 +0000

    Create ServiceFilterAttribute.cs
    Inspired by ASPNET Core ServiceFilter

---
+++ Sharpener.Web/Attributes/ServiceFilterAttribute.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AdaptivToolkitWeb.Attributes
+{
+    public class ServiceFilterAttribute : ActionFilterAttribute
+    {
+        private readonly Type _typeToInject;
+        private ActionFilterAttribute _typeInstance;
+
+        public ServiceFilterAttribute(Type typeToInject)
+        {
+            _typeToInject = typeToInject;
+            CreateInstance();
+        }
+
+        private void CreateInstance()
+        {
+            var firstCtor = _typeToInject.GetConstructors().FirstOrDefault();
+            var ctorParamTypes = firstCtor.GetParameters().Select(x => x.ParameterType);
+            var injectedCtorParamValues = ctorParamTypes.Select(x => DependencyResolver.Current.GetService(x)).ToArray();
+            _typeInstance = Activator.CreateInstance(_typeToInject, injectedCtorParamValues) as ActionFilterAttribute;
+        }
+
+        public virtual void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            _typeInstance.OnActionExecuting(filterContext);
+        }
+
+
+        public virtual void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            _typeInstance.OnActionExecuted(filterContext);
+        }
+
+        public virtual void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            _typeInstance.OnResultExecuting(filterContext);
+        }
+
+        public virtual void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            _typeInstance.OnResultExecuted(filterContext);
+        }
+    }
+}
