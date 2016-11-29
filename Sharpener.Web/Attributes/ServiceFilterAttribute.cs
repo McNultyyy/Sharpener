c6017d475d1e01f01e67ac14091841186265e6b2
McNultyyy <william.mcnulty@live.co.uk>
2016-11-29 10:21:23 +0000

    Update ServiceFilterAttribute.cs

--- Sharpener.Web/Attributes/ServiceFilterAttribute.cs
+++ Sharpener.Web/Attributes/ServiceFilterAttribute.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace AdaptivToolkitWeb.Attributes
+namespace Sharpener.Web.Attributes
 {
     public class ServiceFilterAttribute : ActionFilterAttribute
     {
