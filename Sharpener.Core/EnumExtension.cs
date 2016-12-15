b3d51b5716c62881e3e51ed29b90cfa2ec44541d
William <william.mcnulty@live.co.uk>
2016-12-15 21:12:55 +0000

    start the enum extension

---
+++ Sharpener.Core/EnumExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sharpener.Core
+{
+    public static class EnumExtension
+    {
+
+
+        public static IEnumerable<EnumValue> GetValues<TEnum>()
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            foreach (var itemType in Enum.GetValues(typeof(TEnum)))
+            {
+                var value = new EnumValue
+                {
+                    Name = Enum.GetName(typeof(TEnum), itemType),
+                    Value = (int)itemType
+                };
+                yield return value;
+            }
+        }
+
+        public class EnumValue
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+        }
+    }
+
+}
